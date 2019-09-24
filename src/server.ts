2514b963b15b0008b02d45bffaf693c9054367de
KostasRomanidis <kromanidis@yahoo.gr>
2019-09-25 00:36:09 +0300

    minor fix

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 // dotenv.config({ path: ".env" });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     logger.info(`Server mode is ${app.get("env")}`);
