ffe81053b991e5b61a937a7ba4087226e54fd61e
KostasRomanidis <kon.romanidis@gmail.com>
2025-06-01 12:10:23 +0300

    add morgna for http request logging

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import api from "./api/index";
 import compression from "compression";
+import morgan from "morgan";
+import logger from "./config/logger";
 
 class App {
     public express: express.Application;
@@ -10,10 +12,17 @@ class App {
         this.express = express();
         this.setMiddlewares();
         this.setRoutes();
-        // this.setErrorHandler();
     }
 
     private setMiddlewares = (): void => {
+        // Morgan logs HTTP requests using Winston's stream
+        this.express.use(
+            morgan("combined", {
+                stream: {
+                    write: (message) => logger.info(message.trim()),
+                },
+            })
+        );
         this.express.use(cors()); // Allow all origins by default
         this.express.use(express.json());
         this.express.use(express.urlencoded({ extended: false }));
@@ -23,10 +32,6 @@ class App {
     private setRoutes = (): void => {
         this.express.use("/api", api);
     };
-
-    // private setErrorHandler = (): void => {
-    //     this.express.use(errorHandler);
-    // };
 }
 
 export default new App().express;
